5720b68edf9a4d261c4d8293affce42dd5abc820
didi3r <hireme@didierperez.com>
2014-04-29 10:07:36 -0500

    Pininos

--- app/src/main.js
+++ app/src/main.js
@@ -3,23 +3,66 @@ define(function(require, exports, module) {
     'use strict';
     // import dependencies
     var Engine = require('famous/core/Engine');
+    var Surface = require('famous/core/Surface');
     var ImageSurface = require('famous/surfaces/ImageSurface');
     var StateModifier = require('famous/modifiers/StateModifier');
     var Scrollview = require('famous/views/Scrollview');
+    var HeaderFooterLayout = require('famous/views/HeaderFooterLayout');
+    var EdgeSwapper = require('famous/views/EdgeSwapper');
+    var NavigationBar = require('famous/widgets/NavigationBar');
 
     // create the main context
     var mainContext = Engine.createContext();
 
     // your app here
-    var logo = new ImageSurface({
-        size: [200, 200],
-        content: '/content/images/famous_logo.png'
-    });
+    // var logo = new ImageSurface({
+    //     size: [200, 200],
+    //     content: '/content/images/famous_logo.png'
+    // });
 
-    var logoModifier = new StateModifier({
-        origin: [0.5, 0.5]
-    });
+    // var logoModifier = new StateModifier({
+    //     origin: [0.5, 0.5]
+    // });
+    
+    function App() {
+        // create the layout
+        var layout = new HeaderFooterLayout();
 
-    mainContext.add(logoModifier).add(logo);
+        var header = new Surface({
+            size: [undefined, 60],
+            content: 'Hello World',
+            classes: ['top-nav-bar'],
+            options: {
+                lineHeight: 60
+            }
+        });
+
+        // create the navigation bar
+        var navigation = new NavigationBar({
+            buttons: {
+                onClasses: ['top-nav-bar', 'on'],
+                offClasses: ['top-nav-bar', 'off'],
+                inTransition: {curve: 'easeInOut', duration: 150},
+                outTransition: {curve: 'easeInOut', duration: 150}
+            }
+        });
+
+        // create the content area
+        var contentArea = new EdgeSwapper({
+            inTransition: {curve: 'easeOutBounce', duration: 500},
+            outTransition: {duration: 300},
+            overlap: true 
+        });
+
+        // link endpoints of layout to widgets
+        layout.header.add(NavigationBar);
+        layout.content.add(contentArea);
+
+        return layout;
+    };
+    var myApp = new App();
+
+
+    mainContext.add(myApp)
 });
 
